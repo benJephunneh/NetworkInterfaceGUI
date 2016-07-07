5e6b611bec1493130fa117345e0528c2bc53ba5e
benJephunneh <cpmtrevithick@gmail.com>
2016-07-07 18:10:23 -0400

    Initial repo creation with solution, etc.
    This marks the repo initialization.

---
+++ NetworkInterfaceGUI/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NetworkInterfaceGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //textBlock.Text = 
+            //textBlock1.Text = 
+            //textBlock2.Text = 
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
